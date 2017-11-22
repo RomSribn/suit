38d95cad33ba4f7845d87c01500459188fa24aba
Алексей Мирошниченко <leksey@MacBook-Pro-Aleksej.local>
2017-11-22 20:36:07 +0300

    add style loader for prod

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'Containers/App'
-import registerServiceWorker from './registerServiceWorker'
+//import registerServiceWorker from './registerServiceWorker'
 
 import 'normalize.css'
 import './App.styl'
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();

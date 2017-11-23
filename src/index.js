e7053bfb9f2b350277ab59559370859b819d55b1
Koyta <koytaboyta@gmail.com>
2017-11-23 11:31:15 +0300

    resolve conflicts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'Containers/App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
 import 'font-awesome/css/font-awesome.min.css'
 import 'normalize.css'
 import './App.styl'
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();

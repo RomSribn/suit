5307d268fa5a166fa121cc737473c4d306635c46
Koyta <koytaboyta@gmail.com>
2017-11-23 12:05:29 +0300

    temporary hardcoded controls for dummy

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,13 +4,24 @@ import Switch from './Switch'
 import Customize from './Customize'
 import Dummy from '../../containers/Main/Dummy'
 
+import Question from 'Assets/images/svg/question.svg'
+import Show from 'Assets/images/svg/show.svg'
+import More from 'Assets/images/svg/more.svg'
+
 const Main = (props) => {
 
   return (
     <section className="main">
       <Switch />
       <div className="container">
-        <Dummy />
+        <div className="dummy-container">
+          <Dummy />
+          <div className="controls">
+            <div className="circle left"><img src={Question} alt=""/></div>
+            <div className="circle center"><img src={Show} alt=""/></div>
+            <div className="circle right"><img src={More} alt=""/></div>
+          </div>
+        </div>
         <Customize />
       </div>
     </section>

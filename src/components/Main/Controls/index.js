ecbd6dce63bca538580c61a08e914dab257a7180
Koyta <koytaboyta@gmail.com>
2017-11-27 02:59:18 +0300

    container for controls

--- src/components/Main/Controls/index.js
+++ src/components/Main/Controls/index.js
@@ -1,30 +1,22 @@
-import React from 'react'
-import './Controls.styl'
+import React from "react";
 
-class Controls extends React.Component {
-
-  constructor(props) {
-    super(props)
-  }
-
-  render () {
-    return (
-      <div className={`controls controls--${this.props.color}` }>
-        <div className="circle left">
-          <img src={this.props.leftIcon} alt=""/>
-          <span className="text">{this.props.leftText}</span>
-        </div>
-        <div className="circle center">
-          <img src={this.props.centerIcon} alt=""/>
-          <span className="text">{this.props.centerText}</span>
-        </div>
-        <div className="circle right">
-          <img src={this.props.rightIcon} alt=""/>
-          <span className="text">{this.props.rightText}</span>
-        </div>
+const Controls = props => {
+  return (
+    <div className={`controls controls--${props.color}`}>
+      <div className="circle left">
+        <img src={props.leftIcon} alt="" />
+        <span className="text">{props.leftText}</span>
       </div>
-    )
-  }
-}
+      <div className="circle center">
+        <img src={props.centerIcon} alt="" />
+        <span className="text">{props.centerText}</span>
+      </div>
+      <div className="circle right">
+        <img src={props.rightIcon} alt="" />
+        <span className="text">{props.rightText}</span>
+      </div>
+    </div>
+  );
+};
 
-export default Controls
+export default Controls;

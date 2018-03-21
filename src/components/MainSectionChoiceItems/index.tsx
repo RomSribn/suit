4301208ed63062e83f08e0aea1dc35eb4d28d3bf
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2018-03-21 21:39:26 +0300

    ChoiceItems component added

---
+++ src/components/MainSectionChoiceItems/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import * as ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { TRANSITION_DUARAION } from '../../config/constants';
+
+class ChoiceItems extends React.PureComponent<ChoiceItemsProps> {
+    static defaultProps = {
+        items: [],
+        basicRoute: '/',
+    };
+    render() {
+        const {
+            items,
+            basicRoute,
+        } = this.props;
+        return (
+        <ReactCSSTransitionGroup
+            transitionName="height-fade-in"
+            transitionEnterTimeout={TRANSITION_DUARAION}
+            transitionLeaveTimeout={TRANSITION_DUARAION}
+        >
+            {items.map(
+            (item: SubgroupChoiceItem) => (
+                    <Link
+                        to={`${basicRoute}/${item.link}`}
+                        key={item.id}
+                        style={{
+                            marginBottom: '1.333rem',
+                            display: 'block',
+                        }}
+                    >
+                    <div className="custom">
+                        <span
+                            className="custom__name"
+                            style={{
+                                color: 'black',
+                            }}
+                        >
+                            {item.linkName} {!!item.status && ':'}
+                        </span>
+                        <span className="custom__status">{item.status}</span>
+                    </div>
+                    </Link>
+                )
+        )}
+        </ReactCSSTransitionGroup>
+        );
+        
+    }
+}
+
+export {
+    ChoiceItems,
+};

2e104f7e7b329b49c4ae0f72205bc6fa631e0797
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2018-03-21 21:33:52 +0300

    API_ADRESS added

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,8 +1,8 @@
 import { observable, action } from 'mobx'
 import axios from 'axios'
-import app from './app'
-import { API_ROOT } from '../config/routes'
-import { setIdToken, removeIdToken } from '../utils/apiUtils'
+import { app } from './app'
+import { API_ROOT } from '../config/routes';
+import { setIdToken, removeIdToken } from '../utils/apiUtils';
 
 class user {
   @observable isAuth
@@ -16,7 +16,7 @@ class user {
   }
 
   @action fetchLogin(username, password) {
-    let url = `http://${API_ROOT}/api/auth/login`
+    let url = `${API_ROOT}/api/auth/login`
     this.isFetching = true
     axios.post(url, {
       username,
@@ -24,10 +24,10 @@ class user {
     }).then(response => {
       app.showLoginForm = false
       this.profile = response.data
-      this.isAuth = true
       this.isFetching = false
       setIdToken(response.data.token)
       localStorage.setItem('AuthUser', JSON.stringify(response.data.user))
+      this.isAuth = true      
     }).catch((error) => {
       this.error = error
     })
@@ -35,7 +35,7 @@ class user {
   }
 
   @action logout() {
-    let url = `http://${API_ROOT}/api/auth/logout`
+    let url = `${API_ROOT}/api/auth/logout`
     this.isFetching = true
     axios.get(url, {
       headers: {'x-auth-token': localStorage.getItem('id_token') }

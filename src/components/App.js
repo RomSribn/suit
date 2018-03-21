b29037d49ce0a06c2d49996544f5c89c8c522857
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2018-03-21 20:48:24 +0300

    mobx-dev tools added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Provider } from 'mobx-react'
 import { Router } from 'react-router-dom'
-
+import DevTools from 'mobx-react-devtools';
 import stores from '../stores'
 import history from '../history'
 
 import Wrapper from './Wrapper'
 
-if(!!localStorage.getItem('AuthUser')) {
-  history.push('/login')
-}
-
 const App = ({...props}) => {
-  return <Provider {...stores}>
-    <Router history={history}>
-      <Wrapper />
-    </Router>
-  </Provider>
+  return ([
+    <Provider {...stores} key="provider">
+      <Router history={history}>
+        <Wrapper />
+      </Router>
+    </Provider>,
+    process.env.NODE_ENV !== 'production' && <DevTools key="mobx-devtools"/>
+  ]);
 }
 
 export default App

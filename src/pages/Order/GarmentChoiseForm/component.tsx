0e7c2f7ae22546052fda803c49799bb34e7fab09
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2018-03-21 21:52:48 +0300

    GarmentChoiceForm component added

---
+++ src/pages/Order/GarmentChoiseForm/component.tsx
@@ -0,0 +1,199 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import * as classNames from 'classnames';
+import * as ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { TRANSITION_DUARAION } from '../../../config/constants';
+import { loc } from './loc';
+import { ADD, REMOVE } from '../../../stores/garments/garments';
+import { CatalogIntroText } from '../CatalogIntroText';
+
+type MakeCatalogItems = (
+    g: Garments,
+    lang: string,
+    activeGarments: string[],
+    toggle: (g: string) => (e: any ) => void) => React.ReactNode[]; // tslint:disable-line
+const makeCatalogItems: MakeCatalogItems = (garments, lang, activeGarments, toggle) => Object
+    .keys(garments)
+    .map(garment => {
+        return garment !== 'design' ? (
+            <label className="catalog__item" key={garment}>
+                <input
+                    type="checkbox"
+                    name="goods"
+                    checked={activeGarments.includes(garment)}
+                    value={garment}
+                    // onClick={(e) => { e.preventDefault(); }}
+                    onChange={toggle(garment)}
+                />
+
+                <span className="catalog__item-decoration">
+                <ReactCSSTransitionGroup
+                    transitionName="fade-in-absolute"
+                    transitionEnterTimeout={TRANSITION_DUARAION}
+                    transitionLeaveTimeout={TRANSITION_DUARAION}
+                >
+                <span key={lang}>
+                {
+                    garments[garment].titles
+                        ? garments[garment].titles![lang]
+                            ? garments[garment].titles![lang]
+                            : garment
+                        : garment
+                }
+                </span>
+                </ReactCSSTransitionGroup>
+                </span>
+                
+            </label>
+        ) : null;
+    });
+interface State {
+    garmentChoiceFormHeight: number;
+}
+class GarmentChoise extends React.Component<GarmentChoiceFormProps, State> { 
+    static defaultProps = {
+        lang: 'en',
+        garments: {},
+        fetchGarments: () => undefined,
+        toggleGarment: () => () => {}, // tslint:disable-line
+    };
+    constructor(props: GarmentChoiceFormProps) {
+        super(props);
+        this.state = {
+            garmentChoiceFormHeight: 0,
+        };
+    }
+    componentWillMount() {
+        this.props.fetchGarments!();
+    }
+    componentDidMount() {
+        const DOMElemtnt = document.getElementsByClassName('catalog__form')[0];
+        const commonHeight = Array.from(DOMElemtnt.children).reduce((acc, child) => {
+            acc += child.clientHeight;
+            return acc;
+        }, 0);
+        if (commonHeight !== this.state.garmentChoiceFormHeight) {
+            this.setState({
+                garmentChoiceFormHeight: commonHeight,
+            });
+        }
+    }
+    componentDidUpdate() {
+        const DOMElemtnt = document.getElementsByClassName('catalog__form')[0];
+        const commonHeight = Array.from(DOMElemtnt.children).reduce((acc, child) => {
+            acc += child.clientHeight;
+            return acc;
+        }, 0);
+        if (commonHeight !== this.state.garmentChoiceFormHeight) {
+            this.setState({
+                garmentChoiceFormHeight: commonHeight,
+            });
+        }
+    }
+    makeOrder = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        const {
+            activeGarments,
+            makeOrder,
+            // orderStore,
+        } = this.props;
+        if (!activeGarments!.length) {
+            e.preventDefault();
+        } else {
+            makeOrder!(activeGarments!);
+        }
+    }
+    activate = (garment: string) => {
+        this.props.toggleGarment!(ADD)(garment);
+    }
+    remove = (garment: string) => {
+        this.props.toggleGarment!(REMOVE)(garment);
+    }
+    toggle = (garment: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) { 
+            this.activate(garment);
+         } else {
+             this.remove(garment);
+         }
+    }
+    render() {
+        const {
+            lang,
+            garments,
+            catalogFormClassName,
+            activeGarments,
+            isIndexPage,
+        } = this.props;
+        return (
+            <div
+                className={classNames(
+                'catalog',
+                {
+                    ['catalog--top-position']: !isIndexPage,
+                },
+                )}
+            >
+            
+                <form
+                    className={`catalog__form ${catalogFormClassName}`}
+                    style={
+                        isIndexPage
+                         ? {}
+                         : {
+                            maxHeight: this.state.garmentChoiceFormHeight,
+                            transition: 'max-height .3s',
+                            overflow: 'hidden',
+                            }
+                    }
+                >
+                    {   isIndexPage &&
+                        <CatalogIntroText lang={lang!}/>
+                    }
+                    <div className="catalog__form-wrap">
+                        {makeCatalogItems(garments!, lang!, activeGarments!, this.toggle)}
+                    </div>
+                    {   isIndexPage &&
+                        <div className="catalog__submit-bar">
+                            <ReactCSSTransitionGroup
+                                transitionName="fade-in-absolute"
+                                transitionEnterTimeout={TRANSITION_DUARAION}
+                                transitionLeaveTimeout={TRANSITION_DUARAION}
+                            >
+                                <Link
+                                    to="/order/details"
+                                    key={lang}
+                                    onClick={this.makeOrder} 
+                                    className="catalog__submit"
+                                >
+                                    {loc[lang!].submit}
+                                    <div className="catalog__submit-lines-group">
+                                        <div className="catalog__submit-line catalog__submit-line--1" />
+                                        <div className="catalog__submit-line catalog__submit-line--2" />
+                                        <div className="catalog__submit-line catalog__submit-line--3" />
+                                    </div>
+                                    <div className="catalog__submit-frame">
+                                        <svg width="100%" height="100%">
+                                            <rect
+                                                className="catalog__submit-rect catalog__submit-rect--1"
+                                                width="100%"
+                                                height="100%"
+                                            />
+                                            <rect
+                                                className="catalog__submit-rect catalog__submit-rect--2"
+                                                width="100%"
+                                                height="100%"
+                                            />
+                                        </svg>
+                                    </div>
+                                </Link>
+                            </ReactCSSTransitionGroup>
+                        </div>
+                    }
+                </form>
+            </div>
+        );
+    }
+}
+
+export {
+    GarmentChoise,
+};
---
+++ src/pages/Order/GarmentChoiseForm/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { inject, observer } from 'mobx-react';
+import { GarmentChoise as GarmentChoiseComponent } from './component';
+
+@inject(({app, garments: { garments }, routing, order}) => {
+    return {
+        lang: app.lang,
+        garments: garments.garmentsList,
+        fetchGarments: garments.fetchGarmentsList,    
+        toggleGarment: (garment: string) => (action: string) => {
+            garments.toggleGarment(action)(garment);
+        },
+        activeGarments: [...garments.activeGarments],
+        path: routing.location.pathname,
+        order: order.order,
+        makeOrder: order.fetchInitialOrder,
+    };
+})
+@observer
+class GarmentChoise extends React.Component<GarmentChoiceFormProps> {
+    render() {
+        const {
+            lang,
+            catalogFormClassName,
+            garments,
+            activeGarments,
+            toggleGarment,
+            path,
+            fetchGarments,
+            routes,
+            makeOrder,
+        } = this.props;
+        return (
+            <GarmentChoiseComponent
+                lang={lang}
+                catalogFormClassName={catalogFormClassName}
+                garments={garments}
+                fetchGarments={fetchGarments}
+                activeGarments={activeGarments}
+                routes={routes}
+                toggleGarment={toggleGarment}
+                path={path}
+                makeOrder={makeOrder}
+                isIndexPage={path === routes.index}
+            />
+        );
+    }
+}
+
+export {
+    GarmentChoise,
+};

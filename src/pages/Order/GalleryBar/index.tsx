8fe486713b2b8f4badc3fca32bce309f23cef41b
Sergey Lysenko <lysenko.sergei.developer@gmail.com>
2018-09-09 23:43:23 +0300

    add lazy load to gallery-bar

--- src/pages/Order/GalleryBar/index.tsx
+++ src/pages/Order/GalleryBar/index.tsx
@@ -37,12 +37,12 @@ class GalleryItem extends React.Component<P, S > {
             const image = new Image();
             image.src = imageUrl;
             image.onload = () => {
-                // this.setState({
-                //     load: {
-                //         ...this.state.load,
-                //         success: imageUrl,
-                //     },
-                // });
+                this.setState({
+                    load: {
+                        ...this.state.load,
+                        success: imageUrl,
+                    },
+                });
                 incremetLoadedCount();
             };
             image.onerror = () => {
@@ -68,9 +68,9 @@ class GalleryItem extends React.Component<P, S > {
             img_url_2d: image,
             our_code: id,
         } = this.props.item;
-        // if (!this.state.load.success) {
-        //     return null;
-        // }
+        if (!this.state.load.success) {
+            return null;
+        }
         const orderStore = this.props.orderStore!;
         const active = _.get(orderStore, 'activeElement.our_code', '') === id;
         return (
@@ -148,10 +148,13 @@ const makeGalleryItems: makeGalleryItems = (
 type State = {
     allLoaded: false;
     loadingProgress: number;
+    renderedElementsCount: number;
 };
+
 class GalleryBar extends React.Component<GalleryBarProps, State> {
     loadedCount = 0;
     intervalID: number;
+    isScrolling = false;
     static sizing() {
         try {
             const barWrap = document.getElementById('js-bar-wrap') as HTMLElement,
@@ -210,6 +213,7 @@ class GalleryBar extends React.Component<GalleryBarProps, State> {
         this.state = {
             allLoaded: false,
             loadingProgress: 0,
+            renderedElementsCount: 35,
         };
     }
     componentDidMount() {
@@ -219,6 +223,38 @@ class GalleryBar extends React.Component<GalleryBarProps, State> {
         window.removeEventListener('resize', GalleryBar.sizing);
         window.clearInterval(this.intervalID);
     }
+
+    handlerItemScrollLoader = (event: ScrollEvent) => {
+        const galleryItem = event.target.querySelector('.gallery__item-blc');
+        const wrapHeight = galleryItem!.offsetHeight,
+              wrapWidth = galleryItem!.offsetWidth,
+              barWrapWidth = wrapWidth - wrapHeight - 2,
+              containerHeight = event.target.offsetHeight;
+
+        let divider = 1;
+        if (barWrapWidth / 4 <= 36) {
+            divider = 1;
+        } else if (barWrapWidth / 4 <= 71) {
+            divider = 2;
+        } else if (barWrapWidth / 4 <= 142) {
+            divider = 3;
+        } else if (barWrapWidth / 4 <= 284) {
+           divider = 4;
+        }
+
+        const itterStep = 10;
+        const heightOfAllItems = (this.state.renderedElementsCount / divider) * wrapHeight;
+        // // max height for scrolls
+        if ( heightOfAllItems > containerHeight && !this.isScrolling) {
+            this.setState({ 
+                renderedElementsCount: this.state.renderedElementsCount + itterStep,
+            });
+            // optimize ui rerender
+            this.isScrolling = true;
+            setTimeout(() => this.isScrolling = false, 100);
+        }
+    }
+
     render() {
         const {
             items,
@@ -228,14 +264,19 @@ class GalleryBar extends React.Component<GalleryBarProps, State> {
             mouseLeave,
             isMouseOverElement,
         } = this.props;
+        const {
+            renderedElementsCount
+        } = this.state;
+        const activeItems =
+        renderedElementsCount > items.length ? items : items.slice(0, renderedElementsCount);
         return (
-           <div className="gallery__bar" id="js-bar-wrap">
+           <div onScroll={this.handlerItemScrollLoader} className="gallery__bar" id="js-bar-wrap">
                 <div
                     className="gallery__bar-cont"
                     id="js-bar-container"
                 >
                 {makeGalleryItems(
-                    items,
+                    activeItems,
                     setActiveElementIndex,
                     activeElementIndex,
                     this.incremetLoadedCount,

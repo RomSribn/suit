eae7a24c84476c29b658ed1cb4cd8507d1fb7238
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2018-09-09 23:50:32 +0300

    Не отображать вертикальное меню слева.

--- src/containers/Common/index.tsx
+++ src/containers/Common/index.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
+import { inject, observer } from 'mobx-react';
 // import { Paralax } from '../../components/OrderDecorationBlocks';
 import { Navigation } from '../Navigation';
 
 interface CommonProps {
     children?: React.ReactChildren;
+    userStore?: IUserStore;
 }
-const Common = (props: CommonProps) => {
-    return (
-        <div className="application" >
-            {/* <Paralax /> */}
-            { window.location.pathname !== '/login' &&
-            <Navigation />
-            }
-            {props.children}
-        </div>
-    );
-};
+
+@inject(({ user }) => ({
+    userStore: user
+}))
+@observer
+class Common extends React.PureComponent<CommonProps> {
+    render() {
+        const userStore = this.props.userStore!;
+        return (
+            <div className="application" >
+                {/* <Paralax /> */}
+                { window.location.pathname !== '/login' &&
+                    userStore.isAuth &&
+                    <Navigation />
+                }
+                {this.props.children}
+            </div>
+        );
+    }
+}
 
 export {
     Common,

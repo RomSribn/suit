320c06faaf10f925c39a55673102393bb473d441
kazanwin1 <kazanwin@bk.ru>
2018-09-11 19:20:46 +0300

    ORDER33. Из квадратного лоадера сделать обычную прямую (серую)

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -6,16 +6,8 @@ const Spinner = () => {
         <div className="spinner__wrapper">
             <div className="spinner">
                 <svg width="100%" height="100%">
-                    <rect
-                        className="rect rect--1"
-                        width="100%"
-                        height="100%"
-                    />
-                    <rect
-                        className="rect rect--2"
-                        width="100%"
-                        height="100%"
-                    />
+                    <line className="line line--1" x1="0" x2="100%" />
+                    <line className="line line--2" x1="0" x2="100%" />
                 </svg>
             </div>
         </div>

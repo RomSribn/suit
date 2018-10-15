44c9f9b1c4263971d1103a21bad4ee9ae2d804df
bintoll <sapronovkd@gmail.com>
2018-10-15 09:30:25 +0300

    Finished and fixed all issues. LIST7
    Fixed after review
    
    Fixed after review
    
    Fixed null ref error

--- src/components/Table/typings.d.ts
+++ src/components/Table/typings.d.ts
@@ -27,3 +27,55 @@ interface PanelRowProps {
     lang: Lang;
     activeOrderId?: string | null;
 }
+
+interface FilterParams {
+    text: string;
+    type?: filterTypes;
+    selectValues?: ListItem[];
+    onClickFilterItem?(params: FilterParams, inputRef: React.RefObject<HTMLDivElement>, setFilterValue?: SetFilterValueFunction): void;
+    onChange?: (value: any) => void;
+    updatePickerData?(params: FilterParams, inputRef: React.RefObject<HTMLDivElement>, setFilterValue?: SetFilterValueFunction): void;
+    textIsActive?: boolean;
+}
+
+interface ListItem {
+    value: string;
+    text: string;
+}
+
+interface FilterData {
+    options?: ListItem[];
+    setFilterValue: SetFilterValueFunction;
+    title?: string;
+    inputRef: React.RefObject<HTMLDivElement>
+}
+
+interface DatePickerFilterFields {
+    dateFrom: string;
+    dateTo: string;
+}
+
+interface DatePickerFilterFields {
+    dateFrom: string;
+    dateTo: string;
+}
+
+interface ActiveFilterValues {
+    status?: string;
+}
+
+type SetFilterValueFunction = (value: string | DatePickerFilterFields) => void
+
+type filterTypes = 'input' | 'date' | 'select' | 'empty';
+
+interface FilterMethodFilter {
+    id: string,
+    value: string,
+    [key: string]: string
+}
+
+interface FilterMethodRow {
+    date: string,
+    [key: string]: string
+}
+

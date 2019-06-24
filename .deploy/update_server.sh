d7eafa35c618691e61b452d9e327bc8679fef8c8
Aleksei Chepurko <aleksey.chepurko@gmail.com>
2019-06-23 20:13:19 -0500

    поправил скрипт деплоя

--- .deploy/update_server.sh
+++ .deploy/update_server.sh
@@ -3,7 +3,7 @@
 ssh root@$SERVER "
 cd clothes
 git pull
-docker build . -t fiber/clothes --build-arg --build-arg ssh_prv_key=\"$(cat ~/.ssh/id_rsa)\" --build-arg ssh_pub_key=\"$(cat ~/.ssh/id_rsa.pub)\" --build-arg ssh_known_hosts=\"$(cat ~/.ssh/known_hosts)\"
+docker build . -t fiber/clothes --build-arg ssh_prv_key=\"$(cat ~/.ssh/id_rsa)\" --build-arg ssh_pub_key=\"$(cat ~/.ssh/id_rsa.pub)\" --build-arg ssh_known_hosts=\"$(cat ~/.ssh/known_hosts)\"
 docker-compose down
 docker-compose up -d
 docker system prune -f

239cd07aec3d6fcad1e9f29ec070757dcd07aa4f
Aleksei Chepurko <aleksey.chepurko@gmail.com>
2019-08-25 19:06:25 +0300

    поправил стили для прелоаедера

--- src/components/Spinner/StylistInfo/salons/StylistInfo.component.kalasova.tsx
+++ src/components/Spinner/StylistInfo/salons/StylistInfo.component.kalasova.tsx
@@ -6,11 +6,11 @@ const img = require('./profile.jpg');
 
 const StylistInfoComponent: React.FC<StylistInfoComponentProps> = (props) => (
     <div className="styles-info__container">
-        <div className="styles-info__header">
+        <h1 className="styles-info__header">
             <img className="styles-info__header-image" src={img} alt=""/>
-        </div>
+            <div>{translations[props.lang].header.yourStylist}</div>
+        </h1>
         <div className="styles-info__content">
-            <div><b>{translations[props.lang].header.yourStylist}</b></div>
             <div>{translations[props.lang].header.waitForIt}</div>
         </div>
     </div>

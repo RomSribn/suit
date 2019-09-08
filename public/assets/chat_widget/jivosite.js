911ccbe4b1f54f6c7cfac32b46820515843c7be3
Aleksei Chepurko <aleksey.chepurko@gmail.com>
2019-09-09 07:44:11 +1200

    иконка непрочитанных сообщений

--- public/assets/chat_widget/jivosite.js
+++ public/assets/chat_widget/jivosite.js
@@ -1,22 +1,69 @@
+const widgetId = 'jivo_custom_widget';
+
+const cssStylesFromObject = (styleObject) => {
+	const styleString = (
+		Object.entries(styleObject).reduce((styleString, [propName, propValue]) => {
+			return `${styleString}${propName}:${propValue};`;
+		}, '')
+	);
+	return styleString;
+};
+
+let api = window.jivo_api;
+
+let unreadMessagesCountContainer = document.createElement('div');
+unreadMessagesCountContainer.setAttribute('id', 'unread-messages-count-container');
+
+const unreadElementsCountBlockStyles = cssStylesFromObject({
+	position: 'absolute',
+	display: 'flex',
+	'justify-content': 'center',
+	'align-items': 'center',
+	top: '-5.5px',
+	right: '-5.5px',
+	'z-index': 9,
+	background: 'red',
+	width: '22px',
+	height: '22px',
+	'border-radius': '50px',
+	color: 'white',
+	'font-weight': 500,
+});
+
+const updateUnreadCount = () => {
+	const unreadCount = api.getUnreadMessagesCount ? api.getUnreadMessagesCount() : 0;
+	unreadMessagesCountContainer.innerText = unreadCount;
+
+	const targetStyle = unreadCount === 0 ? 'display: none' : unreadElementsCountBlockStyles;
+
+	unreadMessagesCountContainer.setAttribute('style', targetStyle);
+};
 
 /*
 	Коллбек-функция, вызывается сразу после того, как
 	JivoSite окончательно загрузился
 */
 function jivo_onLoadCallback(){
-	// Создаем элемент DIV для ярлыка
+	api = window.jivo_api;
+	const widgetContainer = document.createElement('div');
+	widgetContainer.setAttribute('id', 'jivo_custom_widget-container');
+
 	window.jivo_cstm_widget = document.createElement('div');
 	jivo_cstm_widget.setAttribute('id', 'jivo_custom_widget');
-	document.body.appendChild(jivo_cstm_widget);
+
+
+	widgetContainer.appendChild(jivo_cstm_widget);
+	widgetContainer.appendChild(unreadMessagesCountContainer);
+	document.body.appendChild(widgetContainer);
 	
 	// Добавляем обработчик клика по ярлыку - чтобы при клике разворачивалось окно
 	jivo_cstm_widget.onclick = function(){
-		jivo_api.open();
+		api.open();
 	}
 	
 	// Изменяем CSS класс, если есть операторы в онлайне
-	if (jivo_config.chat_mode == "online"){
-		jivo_cstm_widget.setAttribute("class", "jivo_online");
+	if (jivo_config.chat_mode === 'online'){
+		jivo_cstm_widget.setAttribute('class', 'jivo_online');
 	}
 	
 	// Теперь можно показать ярлык пользователю
@@ -38,3 +85,8 @@ function jivo_onClose(){
 	if (jivo_cstm_widget)
 		jivo_cstm_widget.style.display = 'block';
 }
+
+function jivo_onChangeState(state) {
+	updateUnreadCount();
+}
+

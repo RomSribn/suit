87b8fdb0c5505e5a63dbbc2ebc168ca308aee24d
Aleksei Chepurko <aleksey.chepurko@gmail.com>
2019-09-23 00:30:50 +0300

    дефолтная страница - стрнаница выбора ткани

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -75,7 +75,7 @@ class Wrapper extends Component {
                 dummyWasRendered = true;
                 this.showSpinner();
               }
-              return <Redirect to={routes.order} />;
+              return <Redirect to={'order/details/shirt/fabric_ref/fabric'} />;
             }}/>
             {
               !userStore.isAuth && <Route path={`${routes.details}/:garment/fitting/fitting`} component={({match, location}) => {

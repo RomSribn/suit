c69a7410e869793e44e399c21cbab34aea9da05a
kazanwin1 <kazanwin@bk.ru>
2020-06-28 14:39:08 +0300

    fix: ts-lint member-ordering

--- src/pages/Order/SubgroupChoice/typings.d.ts
+++ src/pages/Order/SubgroupChoice/typings.d.ts
@@ -2,14 +2,14 @@ interface SubgroupChoiceProps {
     lang?: Lang;
     data?: SubgroupChoiceItem[];
     SubgroupsStore?: any;
-    fetchGarments?(): void;
     match: Match;
     path?: string;
     order?: Order;
     defaultValues?: OrderItem;
-    setSubgroupTitle?(): void;
-    choiceItem?: OrderPathItem,
-    popOrderPathitem?(): void;
+    choiceItem?: OrderPathItem;
     backLink?: string;
     userStore?: IUserStore;
+    fetchGarments?(): void;
+    setSubgroupTitle?(): void;
+    popOrderPathitem?(): void;
 }

512b68919b2d932102af0e86e931b5220c8d3e85
Fertern <ferternever@gmail.com>
2020-10-04 22:58:38 +0300

    ORDER132 init

--- src/pages/Order/GalleryBar/index.tsx
+++ src/pages/Order/GalleryBar/index.tsx
@@ -6,279 +6,308 @@ import * as classnames from 'classnames';
 import { isMobile, isLandscape } from '../../../utils';
 
 interface P {
-    item: GalleryStoreItem;
-    shownItem: GalleryStoreItem;
-    orderStore?: IOrderStore;
-    filterStore?: IFilterStore;
-    onClick(): void;
-    onMouseEnter(): void;
-    onMouseLeave(): void;
-    incremetLoadedCount(): void;
+  item: GalleryStoreItem;
+  shownItem: GalleryStoreItem;
+  orderStore?: IOrderStore;
+  filterStore?: IFilterStore;
+  onClick(): void;
+  onMouseEnter(): void;
+  onMouseLeave(): void;
+  incremetLoadedCount(): void;
 }
 
-interface S extends ImageLoadState {
-
-}
+interface S extends ImageLoadState {}
 
 // TODO: переделать GalleryItem под observer компоненту и убрать ненужно прокинутые пропсы
-@inject(({
-    order,
-    filterStore
-}) => ({
-        orderStore: order,
-        filterStore
-    })
-)
+@inject(({ order, filterStore }) => ({
+  orderStore: order,
+  filterStore,
+}))
 @observer
-class GalleryItem extends React.Component<P, S > {
-    constructor(props: P) {
-        super(props);
-        this.state = {
-            load: {
-                error: null,
-                success: null,
-            }
-        };
-    }
-    componentDidMount() {
-        try {
-            const { item: { img_url_2d: imageUrl }, incremetLoadedCount } = this.props;
-            const image = new Image();
-            image.src = imageUrl;
-            image.onload = () => {
-                this.setState({
-                    load: {
-                        ...this.state.load,
-                        success: imageUrl,
-                    },
-                });
-                incremetLoadedCount();
-            };
-            image.onerror = () => {
-                incremetLoadedCount();
-            };
-        } catch (e) {
-            this.props.incremetLoadedCount();
-            this.setState({
-                load: {
-                    ...this.state.load,
-                    error: e,
-                },
-            });
-        }
-    }
+class GalleryItem extends React.Component<P, S> {
+  constructor(props: P) {
+    super(props);
+    this.state = {
+      load: {
+        error: null,
+        success: null,
+      },
+    };
+  }
+  componentDidMount() {
+    try {
+      const {
+        item: { img_url_2d: imageUrl, img_url_2d_list },
+        incremetLoadedCount,
+      } = this.props;
+      const image = new Image();
+      image.src = imageUrl;
+      image.onload = () => {
+        this.setState({
+          load: {
+            ...this.state.load,
+            success: imageUrl,
+          },
+        });
+        incremetLoadedCount();
+      };
+      image.onerror = () => {
+        incremetLoadedCount();
+      };
 
-    render() {
-        const {
-            onMouseEnter,
-            onMouseLeave,
-            shownItem,
-            onClick
-        } = this.props;
-        const {
-            img_url_2d: image,
-            our_code: id,
-        } = this.props.item;
-        if (!this.state.load.success) {
-            return null;
-        }
-        // TODO: (KMP) убрать нахуй в стору. Надеюсь, к этому куску говна не надо будет притрагиваиться
-        // только при рефакторе всего проекта
-        if (this.props.filterStore) {
-            const filters = this.props.filterStore.userFilters;
-            const filterNames = Object.keys(filters);
-
-            for (const name in filterNames) {
-                // Если в массиве значений данного фильтра filters[name]
-                // есть такое же значение, как у данного элемента галлереи,
-                // тогда шлем все нахуй
-                if (filters[name] && filters[name].includes(this.props.item[name].filterValue)) {
-                    return null;
-                }
-            }
-        }
-        const orderStore = this.props.orderStore!;
-        const active =
-            _.get(orderStore, 'activeElement.our_code', '') === id ||
-            shownItem.our_code === id;
-        const click = () => {
-            this.props.filterStore!.closeFilter();
-            onClick();
-        };
-        return (
-            <div
-                onClick={click}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-                className={classnames(
-                    'gallery__item-blc',
-                    { landscape:  isMobile() && isLandscape() }
-                )}
-            >
-                <div
-                    className={classnames(
-                        'gallery__item',
-                        { active }
-                    )}
-                >
-                    <img src={image} alt={`${id}`} />
-                </div>
-            </div>
-        );
+      const imageHover = new Image();
+      imageHover.src = img_url_2d_list[1];
+      imageHover.onload = () => {
+        this.setState({
+          load: {
+            ...this.state.load,
+            success: imageUrl,
+          },
+        });
+        incremetLoadedCount();
+      };
+      imageHover.onerror = () => {
+        incremetLoadedCount();
+      };
+    } catch (e) {
+      this.props.incremetLoadedCount();
+      this.setState({
+        load: {
+          ...this.state.load,
+          error: e,
+        },
+      });
     }
+  }
+
+  render() {
+    const { onMouseEnter, onMouseLeave, shownItem, onClick } = this.props;
+    const {
+      img_url_2d: image,
+      our_code: id,
+      img_url_2d_list,
+    } = this.props.item;
+    if (!this.state.load.success) {
+      return null;
+    }
+    // TODO: (KMP) убрать нахуй в стору. Надеюсь, к этому куску говна не надо будет притрагиваиться
+    // только при рефакторе всего проекта
+    if (this.props.filterStore) {
+      const filters = this.props.filterStore.userFilters;
+      const filterNames = Object.keys(filters);
+
+      for (const name in filterNames) {
+        // Если в массиве значений данного фильтра filters[name]
+        // есть такое же значение, как у данного элемента галлереи,
+        // тогда шлем все нахуй
+        if (
+          filters[name] &&
+          filters[name].includes(this.props.item[name].filterValue)
+        ) {
+          return null;
+        }
+      }
+    }
+    const orderStore = this.props.orderStore!;
+    const active =
+      _.get(orderStore, 'activeElement.our_code', '') === id ||
+      shownItem.our_code === id;
+    const click = () => {
+      this.props.filterStore!.closeFilter();
+      onClick();
+    };
+    return (
+      <div
+        onClick={click}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        className={classnames('gallery__item-blc', {
+          landscape: isMobile() && isLandscape(),
+        })}
+      >
+        <div className={classnames('gallery__item', { active })}>
+          <img
+            src={img_url_2d_list[1]}
+            className="gallery__item--hover-image"
+            alt={`${id}`}
+          />
+          <img
+            src={image}
+            className="gallery__item--main-image"
+            alt={`${id}`}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 const galleryItemsCache: Record<string, React.ReactNode[]> = {};
 type makeGalleryItems = (
-    items: GalleryStoreItems,
-    setActiveElementIndex: (i: number, action?: string, id?: string, fabric?: string) => () => void,
-    setPreviewElementIndex: (elementIndex: number, action?: string) => void,
-    shownItem: GalleryStoreItem,
-    incremetLoadedCount: () => void,
-    isMouseOverElement: boolean,
+  items: GalleryStoreItems,
+  setActiveElementIndex: (
+    i: number,
+    action?: string,
+    id?: string,
+    fabric?: string
+  ) => () => void,
+  setPreviewElementIndex: (elementIndex: number, action?: string) => void,
+  shownItem: GalleryStoreItem,
+  incremetLoadedCount: () => void,
+  isMouseOverElement: boolean
 ) => React.ReactNode[];
 
 const makeGalleryItems: makeGalleryItems = (
-    items,
-    setActiveElementIndex,
-    setPreviewElementIndex,
-    shownItem,
-    incremetLoadedCount,
-    isMouseOverElement,
+  items,
+  setActiveElementIndex,
+  setPreviewElementIndex,
+  shownItem,
+  incremetLoadedCount,
+  isMouseOverElement
 ) => {
-    const cache = items.reduce((acc: string[], item): string[] => {
+  const cache =
+    items
+      .reduce((acc: string[], item): string[] => {
         acc.push(item.our_code);
         return acc;
-    }, []).join('') + shownItem.our_code;
-    if (galleryItemsCache[cache]) {
-        return galleryItemsCache[cache];
-    }
-    const result = items.map((item, elementIndex) => {
-        return (
-            <GalleryItem
-                key={item.fabric_code + item.our_code}
-                item={item}
-                onClick={setActiveElementIndex(elementIndex)}
-                shownItem={shownItem}
-                onMouseEnter={() => {
-                    setPreviewElementIndex(elementIndex, 'enter');
-                }}
-                onMouseLeave={() => {
-                    setPreviewElementIndex(-1, 'leave');
-                }}
-                incremetLoadedCount={incremetLoadedCount}
-            />);
-    });
-    galleryItemsCache[cache] = result;
-    return result;
+      }, [])
+      .join('') + shownItem.our_code;
+  if (galleryItemsCache[cache]) {
+    return galleryItemsCache[cache];
+  }
+  const result = items.map((item, elementIndex) => {
+    return (
+      <GalleryItem
+        key={item.fabric_code + item.our_code}
+        item={item}
+        onClick={setActiveElementIndex(elementIndex)}
+        shownItem={shownItem}
+        onMouseEnter={() => {
+          setPreviewElementIndex(elementIndex, 'enter');
+        }}
+        onMouseLeave={() => {
+          setPreviewElementIndex(-1, 'leave');
+        }}
+        incremetLoadedCount={incremetLoadedCount}
+      />
+    );
+  });
+  galleryItemsCache[cache] = result;
+  return result;
 };
 
 type State = {
-    allLoaded: false;
-    loadingProgress: number;
-    renderedElementsCount: number;
-    isShowedExceptionPopup: boolean;
-    titleSubGroup: string;
-    titleElement: Translations<string> | null;
+  allLoaded: false;
+  loadingProgress: number;
+  renderedElementsCount: number;
+  isShowedExceptionPopup: boolean;
+  titleSubGroup: string;
+  titleElement: Translations<string> | null;
 };
 
 class GalleryBar extends React.Component<GalleryBarProps, State> {
-    loadedCount = 0;
-    isScrolling = false;
-    galleryBar: React.RefObject<HTMLDivElement>;
-    galleryItemElement: HTMLDivElement | null;
+  loadedCount = 0;
+  isScrolling = false;
+  galleryBar: React.RefObject<HTMLDivElement>;
+  galleryItemElement: HTMLDivElement | null;
 
-    incremetLoadedCount = () => {
-        this.loadedCount++;
+  incremetLoadedCount = () => {
+    this.loadedCount++;
+  }
+  constructor(props: GalleryBarProps) {
+    super(props);
+    this.state = {
+      allLoaded: false,
+      loadingProgress: 0,
+      renderedElementsCount: 35,
+      isShowedExceptionPopup: false,
+      titleSubGroup: '',
+      titleElement: null,
+    };
+    this.props.setPreviewElementIndex(
+      this.props.activeElementIndex || 0,
+      'enter'
+    );
+    this.galleryBar = React.createRef();
+  }
+
+  _handleScroll = (event: ScrollEvent) => {
+    if (this.state.renderedElementsCount >= this.props.items.length) {
+      return;
     }
-    constructor(props: GalleryBarProps) {
-        super(props);
-        this.state = {
-            allLoaded: false,
-            loadingProgress: 0,
-            renderedElementsCount: 35,
-            isShowedExceptionPopup: false,
-            titleSubGroup: '',
-            titleElement: null
-        };
-        this.props.setPreviewElementIndex(this.props.activeElementIndex || 0, 'enter');
-        this.galleryBar = React.createRef();
-    }
-
-    _handleScroll = (event: ScrollEvent) => {
-        if (this.state.renderedElementsCount >= this.props.items.length) {
-            return;
-        }
-        if (!this.galleryBar.current) {
-            return;
-        }
-
-        const galleryWrapper = this.galleryBar.current;
-
-        const itterStep = 10;
-
-        if (event.target && event.target.scrollTop > 0.5 * galleryWrapper.offsetHeight) {
-            this.setState({
-                renderedElementsCount: this.state.renderedElementsCount + itterStep,
-            });
-        }
+    if (!this.galleryBar.current) {
+      return;
     }
 
-    throttledHandleScroll = _.throttle(this._handleScroll, 300); // tslint:disable-line
+    const galleryWrapper = this.galleryBar.current;
 
-    handleScroll = (event: ScrollEvent) => {
-        event.persist();
-        this.throttledHandleScroll(event);
+    const itterStep = 10;
+
+    if (
+      event.target &&
+      event.target.scrollTop > 0.5 * galleryWrapper.offsetHeight
+    ) {
+      this.setState({
+        renderedElementsCount: this.state.renderedElementsCount + itterStep,
+      });
     }
+  }
 
-    showExceptionPopup = (titleSubGroup: string, titleElement: Translations<string> | null) => (
-        this.setState({
-            isShowedExceptionPopup: true,
-            titleSubGroup,
-            titleElement
-        })
-    )
+  throttledHandleScroll = _.throttle(this._handleScroll, 300); // tslint:disable-line
 
-    hideExceptionPopup = () => this.setState({ isShowedExceptionPopup: false });
+  handleScroll = (event: ScrollEvent) => {
+    event.persist();
+    this.throttledHandleScroll(event);
+  }
 
-    render() {
-        const {
-            items,
+  showExceptionPopup = (
+    titleSubGroup: string,
+    titleElement: Translations<string> | null
+  ) =>
+    this.setState({
+      isShowedExceptionPopup: true,
+      titleSubGroup,
+      titleElement,
+    })
+
+  hideExceptionPopup = () => this.setState({ isShowedExceptionPopup: false });
+
+  render() {
+    const {
+      items,
+      setActiveElementIndex,
+      setPreviewElementIndex,
+      shownItem,
+      isMouseOverElement,
+    } = this.props;
+    const { renderedElementsCount } = this.state;
+    const activeItems =
+      renderedElementsCount > items.length
+        ? items
+        : items.slice(0, renderedElementsCount);
+    return (
+      <div
+        className="gallery__bar"
+        onScroll={this.handleScroll}
+        id="js-bar-wrap"
+      >
+        <div
+          ref={this.galleryBar}
+          className="gallery__bar-cont"
+          id="js-bar-container"
+        >
+          {makeGalleryItems(
+            activeItems,
             setActiveElementIndex,
             setPreviewElementIndex,
             shownItem,
-            isMouseOverElement,
-        } = this.props;
-        const {
-            renderedElementsCount,
-        } = this.state;
-        const activeItems =
-        renderedElementsCount > items.length ? items : items.slice(0, renderedElementsCount);
-        return (
-            <div
-                className="gallery__bar"
-                onScroll={this.handleScroll}
-                id="js-bar-wrap"
-            >
-                <div
-                    ref={this.galleryBar}
-                    className="gallery__bar-cont"
-                    id="js-bar-container"
-                >
-                {makeGalleryItems(
-                    activeItems,
-                    setActiveElementIndex,
-                    setPreviewElementIndex,
-                    shownItem,
-                    this.incremetLoadedCount,
-                    isMouseOverElement,
-                )}
-                </div>
-            </div>
-        );
-    }
+            this.incremetLoadedCount,
+            isMouseOverElement
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-export {
-    GalleryBar,
-};
+export { GalleryBar };
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,50 +1,45 @@
-
 interface NavigationRoutes {
-    index: string;
-    order: string;
-    panel: string;
-    customersList: string;
-    calendar: string;
-    store: string;
-    analytics: string;
-    settings: string;
-    ordersList: string;
+  index: string;
+  order: string;
+  panel: string;
+  customersList: string;
+  calendar: string;
+  store: string;
+  analytics: string;
+  settings: string;
+  ordersList: string;
 }
 
 interface Routes {
-    details: string;
+  details: string;
 }
-
 
 /**
  * Описываем что-то с бэка
  */
 interface Match {
-    params: {
-        [key: string]: string;
-    }
-    path: string;
-    url: string;
-    isExact: boolean;
+  params: {
+    [key: string]: string;
+  };
+  path: string;
+  url: string;
+  isExact: boolean;
 }
 
 interface Location {
-    pathname: string;
+  pathname: string;
 }
 
+type Translations<Locale> = {
+  en: Locale;
+  ru: Locale;
+};
 
-type Translations <Locale> = {
-    en: Locale,
-    ru: Locale,
-}
-
-type MakeLocale<Locale> =
-    Translations<Locale> |
-    (() => Translations<Locale>)
+type MakeLocale<Locale> = Translations<Locale> | (() => Translations<Locale>);
 
 interface OrderPathItem {
-    value: string;
-    link?: string;
+  value: string;
+  link?: string;
 }
 
 type OrderPath = OrderPathItem[];
@@ -52,49 +47,49 @@ type OrderPath = OrderPathItem[];
 type Lang = 'ru' | 'en';
 
 interface AppStore {
-    lang: Lang;
-    orderPath: OrderPath;
+  lang: Lang;
+  orderPath: OrderPath;
 }
 
 interface GarmentLocale {
-    name: string;
+  name: string;
 }
 interface Garment {
-    titles?: MakeLocale<GarmentLocale>;
-    isSubclear?: boolean;
-    id: string;
-    name: string;
-  }
+  titles?: MakeLocale<GarmentLocale>;
+  isSubclear?: boolean;
+  id: string;
+  name: string;
+}
 
 interface Garments {
-    [key: string]: Garment;
+  [key: string]: Garment;
 }
 interface ImageLoadState {
-    load: {
-        error: string | null,
-        success: string | null;
-    },
+  load: {
+    error: string | null;
+    success: string | null;
+  };
 }
 
 interface ServerFilter {
-    name: string;
-    id: number;
-    title: Translations<string>;
-    value: string;
-    valueTitle: Translations<string>;
-  }
+  name: string;
+  id: number;
+  title: Translations<string>;
+  value: string;
+  valueTitle: Translations<string>;
+}
 
 type ServerFilters = Record<string, ServerFilter[]>;
 
 interface FilterValue {
-    id?: number;
-    valueTitle: Translations<string>;
-    value: string;
+  id?: number;
+  valueTitle: Translations<string>;
+  value: string;
 }
 interface Filter {
-    name: string;
-    title: Translations<string>;
-    values: FilterValue[];
+  name: string;
+  title: Translations<string>;
+  values: FilterValue[];
 }
 
 type Filters = Record<string, Filter>;
@@ -102,44 +97,45 @@ type Filters = Record<string, Filter>;
 type UserFilters = Record<string, string[]>;
 
 interface GalleryStoreItem {
-    prevActiveItem?: string | null;
-    our_code: string;
-    title: Translations<string>;
-    description: Translations<string>;
-    exception: string[];
-    is_input: boolean;
-    is_item_clear: boolean;
-    image: string | null;
-    img_url_2d: string;
-    image_url_3d: string | null;
-    price_category: string | null;
-    code: string;
-    price: Translations<number>;
-    defaultItem: boolean;
-    fabric_code: string;
-    elementInfo: {
-        garment: string;
-        group: 'fitting' | 'design' | 'fabric_ref';
-        subGroup: string;
-    }
+  prevActiveItem?: string | null;
+  our_code: string;
+  title: Translations<string>;
+  description: Translations<string>;
+  exception: string[];
+  is_input: boolean;
+  is_item_clear: boolean;
+  image: string | null;
+  img_url_2d: string;
+  img_url_2d_list: string;
+  image_url_3d: string | null;
+  price_category: string | null;
+  code: string;
+  price: Translations<number>;
+  defaultItem: boolean;
+  fabric_code: string;
+  elementInfo: {
+    garment: string;
+    group: 'fitting' | 'design' | 'fabric_ref';
+    subGroup: string;
+  };
 }
 
 type GalleryStoreItems = GalleryStoreItem[];
 
 interface Stores {
-    app: AppStore;
-    routing: any;
-  }
+  app: AppStore;
+  routing: any;
+}
 
 declare class GalleryStore {
-    garment: string;
-    subGroup: string;
-    group: string;
+  garment: string;
+  subGroup: string;
+  group: string;
 }
 
 type ActivePreviewElement = {
-    garment: string;
-    group: string;
-    subGroup: string;
-    value: string;
+  garment: string;
+  group: string;
+  subGroup: string;
+  value: string;
 } | null;

17544bb835ea99321d812460a44878d12520e908
vitvitskyi <vitvitskyi.vd@gmail.com>
2020-12-04 14:58:51 +0200

    merge master

--- src/pages/Order/GroupChoice/index.tsx
+++ src/pages/Order/GroupChoice/index.tsx
@@ -1,26 +1,21 @@
 import * as React from 'react';
-// import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import * as _ from 'lodash';
 import { loc } from './loc';
 import { isMobile, isLandscape, isTablet } from '../../../utils';
 import { inject, observer } from 'mobx-react';
 import { GroupChoiceProps } from './typings';
 import { CommonStores } from '../../../types/commonStores';
-// import { routes } from '../routes';
+import { routes } from '../routes';
 import { Controll, Filter } from '../Filter';
 import './styles.styl';
-import { Navlinks } from '../../../components/Header/HeaderContent/navlinks';
 import { PopUp } from '../../../containers/Popup';
 
-@inject<CommonStores, GroupChoiceProps, {}, unknown>(({order, app, filterStore, user, garments:
-    { Subgroups, garments } }) => {
+@inject<CommonStores, GroupChoiceProps, {}, unknown>(({ app, filterStore, garments: { Subgroups } }) => {
     return {
-        userStore: user,
-        activeGarments: [...garments!.activeGarments],
         subgroupsStore: new Subgroups('shirt'),
         app,
         filterStore,
-        orderStore: order,
     };
 })
 @observer
@@ -45,6 +40,7 @@ class GroupChoice extends React.PureComponent<GroupChoiceProps> {
     }
 
     openModal = () => {
+        console.log('modalIsOpen', this.state.modalIsOpen);
         this.setState({ modalIsOpen: true });
     }
 
@@ -59,18 +55,10 @@ class GroupChoice extends React.PureComponent<GroupChoiceProps> {
         const {
             match: { params: { garment, subgroup, group } },
             order,
-            // backLink,
+            backLink,
             lang,
             subgroupsStore,
-            orderStore,
-            userStore,
-            activeGarments
         } = this.props;
-        let itemsWithOwnFabric: Subgroup[] = [];
-        if (subgroupsStore!.data) {
-            itemsWithOwnFabric = subgroupsStore!.data.design
-                .filter((s) => s.is_allowOwnFabric);
-        }
 
         const fixedGarment = subgroup === 'fitting' ? 'shirt' : garment;
 
@@ -82,99 +70,59 @@ class GroupChoice extends React.PureComponent<GroupChoiceProps> {
 
         if (!choiceItemValue) {
             choiceItemValue =
-            _.get(order, `${fixedGarment}[0].${subgroup}.${group}.title.${lang}`, loc[lang].noStatus);
+                _.get(order, `${fixedGarment}[0].${subgroup}.${group}.title.${lang}`, loc[lang].noStatus);
         }
 
         const itemValue = isMobile() && !isLandscape() && choiceItemValue && choiceItemValue!.length > 15 ||
-            !(isMobile() || (isTablet() && !isLandscape()))
+        !(isMobile() || (isTablet() && !isLandscape()))
             ? choiceItemValue!.slice(0, 12) + '...'
             : choiceItemValue;
         // console.log('subStore.data', subgroupsStore!.data![subgroup]); // tslint:disable-line
         // console.log('group', group); // tslint:disable-line
-        const content = (
-            <>
-                <span className="custom__content">
-                    {isMobile() ?
-                <Navlinks
-                    garment={activeGarments && activeGarments[0] || 'shirt'}
-                    isAuth={userStore!.isAuth}
-                /> : <>
-                    <span className="custom__name">{subgroupsStore!.data
+        const subgroupName = subgroupsStore!.data
             ? subgroupsStore!.data[subgroup]
                 .find((item: Subgroup) => item.subsection_our_code === group).title[lang]
-            : ''}:</span>
-                    <span className="custom__status">{itemValue}</span>
-                </>}
+            : '';
 
-                </span>
+        const content = (
+            <>
+            <span className="custom__content">
+            <span className="custom__name">{subgroupName}:</span>
+            <span className="custom__status">{itemValue}</span>
+            </span>
                 {(subgroup === 'fitting') || (subgroup === 'design') && <span className="custom__control"/>}
             </>
         );
-        const partOfShirtToggleItems = [
-            {
-                subsection_our_code: 'all',
-                title: { en: 'all', ru: 'все' },
-            },
-            ...itemsWithOwnFabric,
-        ];
-
-        const toggle = (
-            <div className="toggle toggle-wrap">
-                {partOfShirtToggleItems.map((item) => (
-                    <span key={item.subsection_our_code} className="toggle__item">
-                        <input
-                            className="toggle__input"
-                            type="radio"
-                            name="allowed_own_fabric"
-                            id={item.subsection_our_code}
-                            value={item.subsection_our_code}
-                            checked={orderStore!.partOfShirtToggle === item.subsection_our_code}
-                            onChange={this.handleChange}
-                        />
-                        <label className="toggle__label" htmlFor={item.subsection_our_code}>
-                            {loc[lang].for} {item.title[lang]}
-                        </label>
-                    </span>
-                ))}
-            </div>
-        );
-
-        const isToggleOnSeparateRow = (isTablet() && !isLandscape());
 
         return (
-<>
-                    <>
-                    <div className="custom custom--open">
+            <Switch>
+                <Route path={routes.fabric}>
+                    <div className="custom custom--open" style={{ overflow: 'hidden', cursor: 'unset' }}>
                         {content}
-                        {!isToggleOnSeparateRow && this.props.app && !this.props.app.isSearchBarOpened && toggle}
-                        <div
-                            className="custom__control_new"
-                            style={{
-                                 flexBasis: this.props.app && this.props.app.isSearchBarOpened ? 'auto' : 0,
-                             }}
-                        >
+                        <div className="custom__control_new">
                             <form
                                 style={{
+                                    display: isMobile() ? 'none' : 'block',
                                     cursor: 'pointer',
-                                    transition: '0.3s',
+                                    transition: '0.5s',
                                     transform: this.props.app && this.props.app.isSearchBarOpened ?
-                                        'unset' : 'translateX(60%)'
+                                        'unset' : 'translateX(74%)'
                                 }}
                                 className="search"
                             >
                                 <div style={{ background: 'transparent' }} className="search__control">
-                                    <span
-                                        style={{
-                                            zIndex: 9999
-                                        }}
-                                        className="search__icon"
-                                        onClick={() => {
-                                            this.props.app!.toggleIsSearchBarOpened();
-                                            if (this.props.filterStore!.isOpen) {
-                                                this.props.filterStore!.toggleOpen();
-                                            }
-                                        }}
-                                    />
+                                <span
+                                    style={{
+                                        zIndex: 9999
+                                    }}
+                                    className="search__icon"
+                                    onClick={() => {
+                                        this.props.app!.toggleIsSearchBarOpened();
+                                        if (this.props.filterStore!.isOpen) {
+                                            this.props.filterStore!.toggleOpen();
+                                        }
+                                    }}
+                                />
                                     <input
                                         style={{
                                             background: 'transparent',
@@ -203,28 +151,25 @@ class GroupChoice extends React.PureComponent<GroupChoiceProps> {
                                     />
                                 </div>
                             </form>
-
                             <Controll openModal={this.openModal}/>
 
                         </div>
                     </div>
-                    {isToggleOnSeparateRow && this.props.withToggle && subgroup !== 'fitting'
-                        && subgroup !== 'design' && toggle}
-
-                        {modalIsOpen && (
-                            <PopUp open={modalIsOpen} onClose={this.closeModal}>
-                                <Filter />
-                            </PopUp>
-                        )}
-                    </>
-                {/* {subgroup === 'fitting' ?
-                    <Link to={backLink}  className="custom custom--openss">
+                </Route>
+                {subgroup === 'fitting' ?
+                    <Link to={backLink}  className="custom custom--open">
                         {content}
                     </Link> :
-                    <Link to={backLink} onClick={this.backClick} className="custom custom--openss">
+                    <Link to={backLink} onClick={this.backClick} className="custom custom--open">
                         {content}
-                    </Link>} */}
-</>
+                    </Link>}
+
+                {modalIsOpen && (
+                    <PopUp open={modalIsOpen} onClose={this.closeModal}>
+                        <Filter />
+                    </PopUp>
+                )}
+            </Switch>
         );
     }
 }

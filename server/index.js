dc620c4aac4c128fd1704afa50fdff8d2fd9fb95
ttaras.tarasovich <ttaras.tarasovich@gmail.com>
2021-01-15 22:43:18 +0200

    ORDER151 enable text compression

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression'); //import to express app
 
 const app = express();
 
+app.use(compression()); //add this as the 1st middleware
 app.use(express.static(path.join(__dirname, '../build')));
-
 
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build/index.html'));

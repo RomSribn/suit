cff1051741b93dd86266d27071455f1acf7446fc
ttaras.tarasovich <ttaras.tarasovich@gmail.com>
2021-01-29 22:09:33 +0200

    ORDER160_2 set service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 import 'font-awesome/css/font-awesome.min.css';
 import 'normalize.css';
 import './App.styl';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+registerServiceWorker();

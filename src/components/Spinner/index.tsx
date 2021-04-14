4301046a5d3fbde35f2cd559c5d029b8e9cf6e1d
andreyname <andreyname@gmail.com>
2021-04-14 17:08:28 +0300

    ORDER206 use react progress bar for slowing status showing

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
 import { StylistInfo } from './StylistInfo';
+import ProgressBar from '@ramonak/react-progress-bar';
 import './styles.styl';
 
-const LinearSpinner = ({ progress, total }: ILinearSpinnerProps) => {
-  const percentage = `${(progress * 100) / total}%`;
+const LinearSpinner = ({ progress = 0, total }: ILinearSpinnerProps) => {
+  let totalValid = total;
+  if (!totalValid) {
+    totalValid = 1;
+  }
+  const percent = (progress * 100) / totalValid;
+
   return (
-    <div className="spinner">
-      <svg width="100%" height="100%">
-        <line className="line line--1" x1="0" x2="100%" />
-        <line className="line line--2 line--static" x1="0" x2={percentage} />
-      </svg>
+    <div className="spinner-linear">
+      <ProgressBar
+        completed={percent}
+        bgColor="#000"
+        height="2px"
+        borderRadius="0"
+        labelAlignment="outside"
+        isLabelVisible={false}
+        baseBgColor="#b7b7b7"
+        labelColor="#000000"
+      />
     </div>
   );
 };
